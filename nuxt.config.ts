187325e0004ebdd43afb06bb009f1c420eee01de
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-10-17 11:37:26 +0200

    Change my config for client only rendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,15 @@ import { usePageModifiers } from "./composables/usePageModifiers";
 import type { Page } from "./types/logicTypes";
 
 export default defineNuxtConfig({
+  ssr: false, // Disattiva il rendering lato server
+  target: "static", // Se è un'app interna, questa opzione va bene
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  plugins: ["~/plugins/pinia"],
-  css: ["bootstrap/dist/css/bootstrap.css"],
   plugins: [
+    "~/plugins/pinia",
     { src: "~/plugins/bootstrap.client.ts", mode: "client" }, // Importa Bootstrap JS solo sul client
   ],
+  css: ["bootstrap/dist/css/bootstrap.css"],
   router: {
     middleware: ["auth"], // Applica il middleware "auth" globalmente
   },
